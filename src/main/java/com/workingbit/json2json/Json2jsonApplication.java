cee7b299d1c3b40775116309712af80c5512facb
Aleksei Popryadukhin <aleksey.p10n@gmail.com>
2018-11-02 11:25:19 +0300

    Initial commit

---
+++ src/main/java/com/workingbit/json2json/Json2jsonApplication.java
@@ -0,0 +1,12 @@
+package com.workingbit.json2json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Json2jsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Json2jsonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/workingbit/json2json/Json2jsonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.workingbit.json2json;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Json2jsonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
