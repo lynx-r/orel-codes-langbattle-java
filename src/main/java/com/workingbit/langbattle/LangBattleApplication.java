fe9a86f17dbbac9908c7b7c9facae8c1bd8aa285
Aleksei Popryadukhin <aleksey.p10n@gmail.com>
2018-11-18 13:26:37 +0300

    apache commons-codec for md5

--- src/main/java/com/workingbit/langbattle/LangBattleApplication.java
+++ src/main/java/com/workingbit/langbattle/LangBattleApplication.java
@@ -1,5 +1,6 @@
 package com.workingbit.langbattle;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
@@ -7,11 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
 import java.util.Map;
 
 import static java.lang.String.format;
@@ -29,19 +28,14 @@ public class LangBattleApplication {
   @PostMapping("/")
   public ResponseEntity<Map<String, Object>> get(@RequestBody() Map<String, Object> req) throws NoSuchAlgorithmException {
     var firstName = (String) req.get("first_name");
-    var firstNameWithMd5 = format("%s %s", firstName, getMd5(firstName));
+    var firstNameWithMd5 = format("%s %s", firstName, DigestUtils.md5Hex(firstName));
     var lastName = (String) req.get("last_name");
-    var lastNameWithMd5 = format("%s %s", lastName, getMd5(lastName));
+    var lastNameWithMd5 = format("%s %s", lastName, DigestUtils.md5Hex(lastName));
     var currentDate = FORMATTER.format(ZonedDateTime.now());
     req.put("first_name", firstNameWithMd5);
     req.put("last_name", lastNameWithMd5);
     req.put("current_time", currentDate);
     req.put("say", "Java is the best");
     return ResponseEntity.ok(req);
-  }
-
-  private String getMd5(String str) throws NoSuchAlgorithmException {
-    var md = MessageDigest.getInstance("MD5");
-    return Base64.getEncoder().encodeToString(md.digest(str.getBytes()));
   }
 }
