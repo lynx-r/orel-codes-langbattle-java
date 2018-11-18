a4cc1254f09da75575ab212a7e82e7395df5ca09
Aleksei Popryadukhin <aleksey.p10n@gmail.com>
2018-11-18 12:12:58 +0300

    refactor on maps

--- src/main/java/com/workingbit/langbattle/LangBattleApplication.java
+++ src/main/java/com/workingbit/langbattle/LangBattleApplication.java
@@ -1,8 +1,5 @@
 package com.workingbit.langbattle;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
@@ -15,49 +12,36 @@ import java.security.NoSuchAlgorithmException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Base64;
+import java.util.Map;
 
-@Data
-class JsonRequest {
-  private String id;
-  @JsonProperty("first_name")
-  private String firstName;
-  @JsonProperty("last_name")
-  private String lastName;
-}
-
-@Data
-@AllArgsConstructor
-class JsonResponse {
-  private String id;
-  @JsonProperty("first_name")
-  private String firstName;
-  @JsonProperty("last_name")
-  private String lastName;
-  private String say;
-  @JsonProperty("current_time")
-  private String currentTime;
-}
+import static java.lang.String.format;
 
 @RestController
 @SpringBootApplication
 public class LangBattleApplication {
+
+  private static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss Z");
 
   public static void main(String[] args) {
     SpringApplication.run(LangBattleApplication.class, args);
   }
 
   @PostMapping("/")
-  public ResponseEntity<JsonResponse> get(@RequestBody() JsonRequest req) throws NoSuchAlgorithmException {
-    String firstNameWithMd5 = req.getFirstName() + " " + getMd5(req.getFirstName());
-    String lastNameWithMd5 = req.getLastName() + " " + getMd5(req.getLastName());
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss Z");
-    String now = ZonedDateTime.now().format(formatter);
-    JsonResponse resp = new JsonResponse(req.getId(), firstNameWithMd5, lastNameWithMd5, "Java is the best", now);
-    return ResponseEntity.ok(resp);
+  public ResponseEntity<Map<String, Object>> get(@RequestBody() Map<String, Object> req) throws NoSuchAlgorithmException {
+    var firstName = (String) req.get("first_name");
+    var firstNameWithMd5 = format("%s %s", firstName, getMd5(firstName));
+    var lastName = (String) req.get("last_name");
+    var lastNameWithMd5 = format("%s %s", lastName, getMd5(lastName));
+    var currentDate = FORMATTER.format(ZonedDateTime.now());
+    req.put("first_name", firstNameWithMd5);
+    req.put("last_name", lastNameWithMd5);
+    req.put("current_time", currentDate);
+    req.put("say", "Java is the best");
+    return ResponseEntity.ok(req);
   }
 
   private String getMd5(String str) throws NoSuchAlgorithmException {
-    MessageDigest md = MessageDigest.getInstance("MD5");
+    var md = MessageDigest.getInstance("MD5");
     return Base64.getEncoder().encodeToString(md.digest(str.getBytes()));
   }
 }
